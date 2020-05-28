ca032d24ad2cb24c2b6bc2df76a180b0981f5ff3
HasanSarikaya1 <60621850+HasanSarikaya1@users.noreply.github.com>
2020-05-28 17:47:11 +0300

    Add files via upload

---
+++ 04MetotUygulaması/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04MetotUygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void maas(int yev,int ssk,int gun)
+        {
+            int brut = gun * yev;
+            int sko = (brut * ssk) / 100;
+            int net = brut - sko;
+            label6.Text = net.ToString();
+            label7.Text = sko.ToString();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int c = Convert.ToInt32(comboBox1.Text);
+            maas(a,c,b);
+        }
+    }
+}
