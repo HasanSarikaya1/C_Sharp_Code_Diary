b5f0f93e3fba68411c79140190740026544c4884
HasanSarikaya1 <60621850+HasanSarikaya1@users.noreply.github.com>
2020-05-28 18:24:04 +0300

    Add files via upload

---
+++ 18KoleksiyonSınıfları/18KoleksiyonSınıfları(Console)/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace _18KoleksiyonSınıfları_Console_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Queuedeki Ana mantık olan ilk giren ilk çıkar ,son giren son çıkar mantığının uygulamalı halidir.
+            /* Queue ku = new Queue(6);
+             ku.Enqueue("Hasan");
+             ku.Enqueue("İsmail");
+             ku.Enqueue("Enes");
+             ku.Enqueue("Mert");
+             ku.Enqueue(123);
+             ku.Enqueue(false);
+             Console.WriteLine("Çıkan Eleman {0}", ku.Dequeue().ToString());
+             Console.WriteLine("Çıkan Eleman {0}", ku.Dequeue().ToString());
+             Console.WriteLine("Çıkan Eleman {0}", ku.Dequeue().ToString());
+             Console.WriteLine("----------------");
+             IEnumerator dizi = ku.GetEnumerator();
+             while (dizi.MoveNext())
+             {
+                 Console.WriteLine("Güncel eleman {0}",dizi.Current.ToString());
+             }
+             Console.ReadKey();
+             */
+
+
+
+            //Stackdeki Ana mantık olan son giren ilk çıkar,ilk çıkan son girer mantığının uygulamalı halidir.Yani bir eleman stack'a üstten katılır
+            // ve yığını yine üstten terk eder.
+            /*Stack sta = new Stack();
+            sta.Push("Hasan");
+            sta.Push("İsmail");
+            sta.Push("Enes");
+            sta.Push("Mert");
+            sta.Push(43);
+            sta.Push(true);
+            Console.WriteLine("Çıkan Eleman {0}", sta.Pop().ToString());
+            Console.WriteLine("Çıkan Eleman {0}", sta.Pop().ToString());
+            Console.WriteLine("Çıkan Eleman {0}", sta.Pop().ToString());
+            IEnumerator dizi = sta.GetEnumerator();
+            while (dizi.MoveNext())
+            {
+                Console.WriteLine("Güncel Eleman {0}",dizi.Current.ToString());
+            }
+            Console.ReadKey();
+            */
+
+
+
+
+
+            //Hashtable koleksiyon sınıfında veriler key-value çiftleri şeklinde tutulmaktadır.Hashtable koleksiyon sınıflarında key ve value değerleri herhangi bir
+            // veri türünde olabilir.Aşağıdaki örnek hashtable'ın ana mantığının uygulamalı halidir.
+            /*Hashtable yas = new Hashtable();
+            yas["Ali"] = 42;
+            yas["Serdar"] = 34;
+            yas["Utku"] = 55;
+            foreach (DictionaryEntry element in yas)
+            {
+                string isim = element.Key.ToString();
+                int has = (int)element.Value;
+                Console.WriteLine("İsmi={0}  Yaşı={1}",isim,has);
+            }
+            Console.ReadKey();
+            */
+
+
+
+            //Hashtable ile aynıdır tek farkı ise anahtarlar dizisinin her zaman sıralanmış olmasıdır.
+            /* SortedList yas = new SortedList();
+            yas["Ali"] =42;
+            yas["Serdar"] = 34;
+            yas["Utku"] = 35;
+            foreach  (DictionaryEntry element in yas)
+            {
+                string isim = element.Key.ToString();
+                int has = (int)element.Value;
+                Console.WriteLine("İsmi={0}  Yaşı={1}", isim, has);
+            }
+            Console.ReadKey();
+            */
+        }
+    }
+}
---
+++ 18KoleksiyonSınıfları/18KoleksiyonSınıfları(Form)/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace _18KoleksiyonSınıfları_Form_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ArrayList siniflistesi = new ArrayList();
+            siniflistesi.Add("Erzurum");
+            siniflistesi.Add(25);
+            siniflistesi.Add(true);
+            textBox1.Text = siniflistesi[0].ToString();
+        }
+        ArrayList ortalama = new ArrayList();
+        int toplam = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            ortalama.Add(Convert.ToInt32(textBox2.Text));
+            textBox2.Clear();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (int a in ortalama)
+            {
+                toplam = toplam + a;
+            }
+            label1.Text = (toplam / ortalama.Count).ToString();
+        }
+        Queue ku = new Queue();
+        string[,] hasta = new string[100,4];
+        int satir = 0;
+        string tc, stc;
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            try
+            {
+                tc = ku.Dequeue().ToString();
+                for (int i = 0; i < 100; i++)
+                {
+                    if (tc == hasta[i, 0])
+                    {
+                        listBox1.Items.Add("İçerideki Hasta : " + hasta[i, 1] + " " + hasta[i,2]);
+                        break;
+                    }
+                }
+                stc = ku.Peek().ToString();
+                for (int i = 0; i < 100; i++)
+                {
+                    if (stc==hasta[i,0])
+                    {
+                        listBox1.Items.Add("Sıradaki Hasta : " + hasta[i, 1] + " " + hasta[i, 2]);
+                        break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ku.Enqueue(textBox3.Text);
+            hasta[satir, 0] = textBox3.Text;
+            hasta[satir, 1] = textBox4.Text;
+            hasta[satir, 2] = textBox5.Text;
+            hasta[satir, 3] = comboBox1.Text;
+            satir++;
+            textBox3.Clear(); 
+            textBox4.Clear(); 
+            textBox5.Clear(); 
+            comboBox1.Text = ""; 
+            textBox3.Focus();
+            MessageBox.Show("Kayıt İşlemi Başarıyla tamamlandı.");
+        }
+    }
+}
