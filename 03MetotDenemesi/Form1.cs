363aa14967ded0b12434bfcd36baa2db4d2a94b6
HasanSarikaya1 <60621850+HasanSarikaya1@users.noreply.github.com>
2020-05-28 17:46:05 +0300

    Add files via upload

---
+++ 03MetotDenemesi/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03MetotDenemesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void islem()
+        {
+            int uzun, kisa, sonuc;
+            uzun = Convert.ToInt32(textBox1.Text);
+            kisa = Convert.ToInt32(textBox2.Text);
+            sonuc = uzun + kisa * 2;
+            button1.Text = sonuc.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            islem();
+        }
+    }
+}
